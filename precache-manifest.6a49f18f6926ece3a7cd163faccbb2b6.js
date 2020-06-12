dd10ca7192a970860c6dfd3ddc902b499db06d44
kalwanipankaj211 <kalwanipankaj@gmail.com>
2020-06-12 09:05:26 +0530

    Updates

--- precache-manifest.6a49f18f6926ece3a7cd163faccbb2b6.js
+++ precache-manifest.6a49f18f6926ece3a7cd163faccbb2b6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "90b2ca93182ce2019200c292d65ed9ac",
+    "revision": "8d562dd42d25e8d6fca4c62305748b08",
     "url": "/kalwanipankaj211/monsters-rolodex.git/index.html"
   },
   {
-    "revision": "8e28685426827b4c9962",
-    "url": "/kalwanipankaj211/monsters-rolodex.git/static/css/main.fb1a206c.chunk.css"
+    "revision": "bd207838664650c8c597",
+    "url": "/kalwanipankaj211/monsters-rolodex.git/static/css/main.85a63cac.chunk.css"
   },
   {
     "revision": "0247f29e2cddb6f90d33",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/kalwanipankaj211/monsters-rolodex.git/static/js/2.6a5852ca.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8e28685426827b4c9962",
+    "revision": "bd207838664650c8c597",
     "url": "/kalwanipankaj211/monsters-rolodex.git/static/js/main.a7d0e823.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kalwanipankaj211/monsters-rolodex.git/precache-manifest.03285f9977a03f08386dd385ebd440f7.js"
+  "/kalwanipankaj211/monsters-rolodex.git/precache-manifest.6a49f18f6926ece3a7cd163faccbb2b6.js"
 );
 
 self.addEventListener('message', (event) => {
