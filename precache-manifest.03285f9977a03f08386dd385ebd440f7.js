1de3eff2ac643e8d8e3e755eaad4fdd595718a69
kalwanipankaj211 <kalwanipankaj@gmail.com>
2020-06-12 08:29:57 +0530

    Updates

---
+++ precache-manifest.03285f9977a03f08386dd385ebd440f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b2ca93182ce2019200c292d65ed9ac",
+    "url": "/kalwanipankaj211/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "8e28685426827b4c9962",
+    "url": "/kalwanipankaj211/monsters-rolodex.git/static/css/main.fb1a206c.chunk.css"
+  },
+  {
+    "revision": "0247f29e2cddb6f90d33",
+    "url": "/kalwanipankaj211/monsters-rolodex.git/static/js/2.6a5852ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kalwanipankaj211/monsters-rolodex.git/static/js/2.6a5852ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e28685426827b4c9962",
+    "url": "/kalwanipankaj211/monsters-rolodex.git/static/js/main.a7d0e823.chunk.js"
+  },
+  {
+    "revision": "74f04300dba07737ce5c",
+    "url": "/kalwanipankaj211/monsters-rolodex.git/static/js/runtime-main.82c99229.js"
+  }
+]);
